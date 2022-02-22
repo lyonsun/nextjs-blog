d2f30a67742b3434a89a6c26df02792b9e29c7b8
Liang Sun <liang@kokku.com>
2022-02-23 01:11:56 +0200

    change primary color from violet to indigo.

--- elements/hero.js
+++ elements/hero.js
@@ -5,16 +5,16 @@ export default function Hero() {
         <section className="bg-[#f9f9fc] hero">
             <div className="relative px-5 py-8 max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-y-10 items-center">
                 <div className="hero__text max-w-md mx-auto md:mx-0 text-center md:text-left">
-                    <h1 className="uppercase text-violet-600 font-medium">Liang Sun</h1>
+                    <h1 className="uppercase text-indigo-600 font-medium">Liang Sun</h1>
                     <div className="py-8 font-bold text-4xl leading-[60px] lg:text-6xl lg:leading-[78px] text-[#333333]">
                         Hello, my name's Liang. I'm web developer.
                     </div>
                     <div className="hero__buttons inline-flex gap-4 flex-col lg:flex-row">
                         <Link href="mailto:sunly917@gmail.com">
-                            <a className="uppercase px-10 py-6 text-white bg-violet-600 rounded-full text-sm font-medium hover:outline hover:outline-1 hover:outline-violet-600 hover:text-violet-600 hover:bg-white transition-colors ease-linear duration-300"><i className="fa-regular fa-envelope mr-2"></i> CONTACT ME</a>
+                            <a className="uppercase px-10 py-6 text-white bg-indigo-600 rounded-full text-sm font-medium hover:outline hover:outline-1 hover:outline-indigo-600 hover:text-indigo-600 hover:bg-white transition-colors ease-linear duration-300"><i className="fa-regular fa-envelope mr-2"></i> CONTACT ME</a>
                         </Link>
                         <Link href="/cv/curriculum-vitae-x.pdf">
-                            <a className="uppercase px-10 py-6 text-[#333333] text-sm font-medium rounded-full hover:bg-violet-600 hover:text-white transition-colors ease-linear duration-300" download target='__blank'><i className="fa-solid fa-download mr-2"></i> DOWNLOAD CV</a>
+                            <a className="uppercase px-10 py-6 text-[#333333] text-sm font-medium rounded-full hover:bg-indigo-600 hover:text-white transition-colors ease-linear duration-300" download target='__blank'><i className="fa-solid fa-download mr-2"></i> DOWNLOAD CV</a>
                         </Link>
                     </div>
                 </div>
