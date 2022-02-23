f4c0d01058f71ac245f1e688bb250d3c5f37a7b1
Liang Sun <liang@kokku.com>
2022-02-23 21:03:09 +0200

    minor fix to hero element with transitions.

--- elements/hero.js
+++ elements/hero.js
@@ -3,13 +3,13 @@ import Link from "next/link"
 export default function Hero() {
     return (
         <section className="bg-[#f9f9fc] hero">
-            <div className="relative px-5 py-8 max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-y-10 items-center">
+            <div className="relative px-5 py-8 max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-y-10 items-center transition-all ease-linear duration-300">
                 <div className="hero__text max-w-md mx-auto md:mx-0 text-center md:text-left">
                     <h1 className="uppercase text-indigo-600 font-medium">Liang Sun</h1>
-                    <div className="py-8 font-bold text-4xl leading-[60px] lg:text-6xl lg:leading-[78px] text-[#333333]">
-                        Hello, my name's Liang. I'm web developer.
+                    <div className="py-8 font-bold text-4xl leading-[60px] lg:text-6xl lg:leading-[78px] text-[#333333] transition-all ease-linear duration-300">
+                        Hello, my name is Liang. I'm web developer.
                     </div>
-                    <div className="hero__buttons inline-flex gap-4 flex-col lg:flex-row">
+                    <div className="hero__buttons inline-flex gap-4 flex-col lg:flex-row transition-all ease-linear duration-300">
                         <Link href="mailto:sunly917@gmail.com">
                             <a className="uppercase px-10 py-6 text-white bg-indigo-600 rounded-full text-sm font-medium hover:outline hover:outline-1 hover:outline-indigo-600 hover:text-indigo-600 hover:bg-white transition-colors ease-linear duration-300"><i className="fa-regular fa-envelope mr-2"></i> CONTACT ME</a>
                         </Link>
